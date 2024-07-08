4008596485a7daa8241a10be6ab31942029bb972
CDAGaming <cstack2011@yahoo.com>
2024-07-08 13:31:08 -0500

    [Port] Update to b1.2_02 (v1.0.5)

--- common/src/main/java/com/gitlab/cdagaming/unilib/utils/gui/integrations/ExtendedScreen.java
+++ common/src/main/java/com/gitlab/cdagaming/unilib/utils/gui/integrations/ExtendedScreen.java
@@ -834,7 +834,7 @@ public class ExtendedScreen extends GuiScreen {
     }
 
     @Override
-    public void drawWorldBackground(int tint) {
+    public void func_567_a(int tint) {
         renderCriticalData();
     }
 
