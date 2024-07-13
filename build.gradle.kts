0a683c5b0675a59f040be853b667180b7d9393f4
CDAGaming <cstack2011@yahoo.com>
2024-07-12 19:09:47 -0500

    [Port] Update to special/rift (v1.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -149,6 +149,7 @@ subprojects {
         maven("https://maven.legacyfabric.net/") {
             name = "Legacy Fabric"
         }
+        maven("https://repo.spongepowered.org/maven/")
         // WagYourTail Mavens
         maven("https://maven.wagyourtail.xyz/releases")
         maven("https://maven.wagyourtail.xyz/snapshots")
@@ -174,6 +175,10 @@ subprojects {
     extensions.getByType<UniminedExtension>().minecraft(sourceSets.getByName("main"), true) {
         side(if (isJarMod) "client" else "combined")
         version(mcVersion)
+
+        rift {
+            loader(mcVersion)
+        }
 
         mappings {
             val mcMappings = "mc_mappings"()!!
