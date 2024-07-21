6a62db1a064407bff6cbf6026095b61f9ab91d44
CDAGaming <cstack2011@yahoo.com>
2024-07-21 14:54:39 -0500

    [Port] Update to 1.1.0 (v1.0.1)

--- common/src/main/java/com/gitlab/cdagaming/unilib/utils/WorldUtils.java
+++ common/src/main/java/com/gitlab/cdagaming/unilib/utils/WorldUtils.java
@@ -105,9 +105,9 @@ public class WorldUtils {
         String name = "clear";
         if (world != null) {
             final WorldInfo info = world.getWorldInfo();
-            if (info.isThundering()) {
+            if (info.getIsThundering()) {
                 name = "thunder";
-            } else if (info.isRaining()) {
+            } else if (info.getIsRaining()) {
                 name = "rain";
             } else {
                 name = "clear";
