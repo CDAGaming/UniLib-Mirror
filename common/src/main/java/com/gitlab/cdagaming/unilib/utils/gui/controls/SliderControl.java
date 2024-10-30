9160729685cd187946f1d6e3c2aa7ec8041203b7
CDAGaming <cstack2011@yahoo.com>
2024-10-30 11:36:28 -0500

    [Port] Update to 1.21.4 (v1.0.4)

--- common/src/main/java/com/gitlab/cdagaming/unilib/utils/gui/controls/SliderControl.java
+++ common/src/main/java/com/gitlab/cdagaming/unilib/utils/gui/controls/SliderControl.java
@@ -248,7 +248,7 @@ public class SliderControl extends ExtendedButtonControl {
      */
     @Override
     public void onClick(double mouseX, double mouseY) {
-        if (super.clicked(mouseX, mouseY)) {
+        if (super.isMouseOver(mouseX, mouseY)) {
             setValueFromMouse(mouseX);
         }
     }
