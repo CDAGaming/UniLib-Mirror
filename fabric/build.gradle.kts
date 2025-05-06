a49d3f7ea4d11869affc6bccc71ca2fafbb946c0
CDAGaming <cstack2011@yahoo.com>
2025-05-06 01:18:06 -0500

    [Port] Update to special/reindev (v1.1.0)

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -1,7 +1,3 @@
-import xyz.wagyourtail.unimined.api.mapping.task.ExportMappingsTask
-import xyz.wagyourtail.unimined.api.minecraft.patch.fabric.FabricLikePatcher
-import xyz.wagyourtail.unimined.api.minecraft.task.RemapJarTask
-
 /**
  * Retrieve a Project Property
  */
@@ -24,22 +20,7 @@ val mcVersionLabel: String by extra
 val baseVersionLabel: String by extra
 
 unimined.minecraft {
-    val fabricData: FabricLikePatcher.() -> Unit = {
-        if (accessWidenerFile.exists()) {
-            accessWidener(accessWidenerFile)
-        }
-        loader("fabric_loader_version"()!!)
-        if (isJarMod) {
-            prodNamespace("official")
-            devMappings = null
-        }
-        customIntermediaries = true
-    }
-    if (isModern) {
-        fabric(fabricData)
-    } else {
-        legacyFabric(fabricData)
-    }
+    // N/A
 }
 
 val common: Configuration by configurations.creating
@@ -49,6 +30,8 @@ configurations.compileClasspath.get().extendsFrom(common)
 configurations.runtimeClasspath.get().extendsFrom(common)
 
 dependencies {
+    "jarMod"("local:nsss:${"forge_version"()}")
+
     // Fabric Integrations (1.14+)
     if (isModern) {
         // Required for loading translation data
@@ -85,44 +68,16 @@ tasks.processResources {
     filesMatching(resourceTargets) {
         expand(replaceProperties)
     }
-
-    filesMatching("mappings.srg") {
-        filter { line ->
-            @Suppress("NULL_FOR_NONNULL_TYPE")
-            if (line.startsWith("CL:")) line.replace("/", ".") else null
-        }
-    }
 }
-
-tasks.named<ExportMappingsTask>("exportMappings") {
-    val target = if (isMCPJar) "searge" else (if (!isModern) "mcp" else "mojmap")
-    export {
-        setTargetNamespaces(listOf(target))
-        setSourceNamespace(if (isJarMod) "official" else "intermediary")
-        location = file("$projectDir/src/main/resources/mappings.srg")
-        setType("SRG")
-    }
-}
-tasks.processResources.get().dependsOn(tasks.named("exportMappings"))
 
 tasks.shadowJar {
     mustRunAfter(project(":common").tasks.shadowJar)
     dependsOn(project(":common").tasks.shadowJar)
     from(zipTree(project(":common").tasks.shadowJar.get().archiveFile))
     configurations = listOf(shadowCommon)
-    archiveClassifier.set("dev-shadow")
+    archiveClassifier.set(project.name)
 }
-
-tasks.named<RemapJarTask>("remapJar") {
-    if (isJarMod) {
-        prodNamespace("official")
-    }
-    dependsOn(tasks.shadowJar.get())
-    asJar {
-        inputFile.set(tasks.shadowJar.get().archiveFile)
-        archiveClassifier.set(project.name)
-    }
-}
+tasks.build.get().dependsOn(tasks.shadowJar.get())
 
 tasks.jar {
     archiveClassifier.set("dev")
